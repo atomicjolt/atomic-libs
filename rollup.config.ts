aa02c054b9bd86373270a89e611e6d173841354e
Sean Collings <seanrcollings@gmail.com>
2022-05-24 14:52:42 -0600

    working on getting bundling working

---
+++ rollup.config.ts
@@ -0,0 +1,48 @@
+import { defineConfig } from "rollup";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import dts from "rollup-plugin-dts";
+import scss from "rollup-plugin-scss";
+import { babel } from "@rollup/plugin-babel";
+
+const packageJson = require("./package.json");
+
+export default defineConfig([
+  {
+    external: ["react", "react/jsx-runtime"],
+    input: "src/index.ts",
+    output: [
+      // {
+      //   dir: "dist/cjs",
+      //   format: "cjs",
+      //   sourcemap: true,
+      //   preserveModules: true,
+      // },
+      {
+        dir: "dist",
+        format: "esm",
+        sourcemap: true,
+        preserveModules: true,
+      },
+    ],
+    plugins: [
+      resolve({
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+      }),
+      commonjs(),
+      babel({
+        babelHelpers: "runtime",
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+      }),
+      scss({
+        sourceMap: true,
+        sass: require("sass"),
+      }),
+    ],
+  },
+  {
+    input: "dist/index.d.ts",
+    output: [{ file: "dist/index.d.ts", format: "esm" }],
+    plugins: [dts()],
+  },
+]);
--- src/components/Buttons/Button/index.tsx
+++ src/components/Buttons/Button/index.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import cn from "classnames";
+import Spinner from "../../Loaders/Spinner";
 import "../../general.scss";
 import "./styles.scss";
-import Spinner from "../../Loaders/Spinner";
 
 interface CommonProps {
   /** What to render within the Button */
