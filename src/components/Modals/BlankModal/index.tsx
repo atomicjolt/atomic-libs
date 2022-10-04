470bab80a7ed0e81bd372e755e510401a4f28b4e
sean.collings@atomicjolt.com <sean.collings@atomicjolt.com>
2022-10-03 19:29:39 -0600

    added BlankModal

---
+++ src/components/Modals/BlankModal/index.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { useModal } from "../utils";
+
+export interface BasicModalProps {
+  /** Whether or not the modal is visible */
+  open?: boolean;
+  children?: React.ReactNode;
+}
+
+/** The most basic Modal that atomic-element offers. Makes no assumptiosn or enforcements on how / where
+ * the content of the modal should be rendered.
+ *
+ * Unless you absolutely need to, you are probably better served using one of the other modals provided
+ */
+export default function BasicModal(props: BasicModalProps) {
+  const { open = false, children } = props;
+  const renderModal = useModal(open);
+
+  return renderModal(
+    <div className="aje-modal-background">
+      <div className="aje-modal">{children}</div>
+    </div>
+  );
+}
--- src/components/Modals/Modal/index.tsx
+++ src/components/Modals/Modal/index.tsx
@@ -1,7 +1,7 @@
-import React, { useEffect } from "react";
-import ReactDOM from "react-dom";
-import { modalInitializer, SharedModalData, useModal } from "../utils";
+import React from "react";
+import { modalInitializer, useModal } from "../utils";
 import Button from "../../Buttons/Button";
+import BasicModal from "../BlankModal";
 
 export interface ModalProps {
   /** Whether or not the modal is visible */
@@ -30,56 +30,54 @@ export interface ModalProps {
  *
  * For when you need a large modal with somewhat complex actions.
  * */
-function Modal({
-  open = false,
-  title,
-  children,
-  primaryButton,
-  secondaryButton = "Cancel",
-  primaryAction,
-  secondaryAction,
-  onClose,
-}: ModalProps) {
-  const renderModal = useModal(open);
+function Modal(props: ModalProps) {
+  const {
+    open = false,
+    title,
+    children,
+    primaryButton,
+    secondaryButton = "Cancel",
+    primaryAction,
+    secondaryAction,
+    onClose,
+  } = props;
 
-  return renderModal(
-    <div className="aje-modal-background">
-      <div className="aje-modal">
-        <div className="aje-modal__top">
-          <h2 className="aje-modal__title">{title}</h2>
-          <button
-            className="aje-modal__close"
-            aria-label="close modal"
-            onClick={() => onClose && onClose()}
-          >
-            <i className="material-icons" aria-hidden>
-              close
-            </i>
-          </button>
-        </div>
-        <div className="aje-modal__main">{children}</div>
-        <div className="aje-modal__bottom">
-          {secondaryButton && (
-            <Button
-              variant="secondary"
-              type="button"
-              onClick={() => primaryAction && primaryAction()}
-            >
-              {secondaryButton}
-            </Button>
-          )}
-          {primaryButton && (
-            <Button
-              variant="primary"
-              type="button"
-              onClick={() => secondaryAction && secondaryAction()}
-            >
-              {primaryButton}
-            </Button>
-          )}
-        </div>
+  return (
+    <BasicModal open={open}>
+      <div className="aje-modal__top">
+        <h2 className="aje-modal__title">{title}</h2>
+        <button
+          className="aje-modal__close"
+          aria-label="close modal"
+          onClick={() => onClose && onClose()}
+        >
+          <i className="material-icons" aria-hidden>
+            close
+          </i>
+        </button>
       </div>
-    </div>
+      <div className="aje-modal__main">{children}</div>
+      <div className="aje-modal__bottom">
+        {secondaryButton && (
+          <Button
+            variant="secondary"
+            type="button"
+            onClick={() => primaryAction && primaryAction()}
+          >
+            {secondaryButton}
+          </Button>
+        )}
+        {primaryButton && (
+          <Button
+            variant="primary"
+            type="button"
+            onClick={() => secondaryAction && secondaryAction()}
+          >
+            {primaryButton}
+          </Button>
+        )}
+      </div>
+    </BasicModal>
   );
 }
 
