0d29412b4d7af503b5bd4fbda185009eff5a08bd
Sean Collings <me@seancollings.dev>
2023-10-16 17:08:17 -0600

    fixed playground

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export * from "./components";
 export * from "./hooks";
 export * from "./filter";
-export {
+export type {
   EventHandler as AtomicElementsEventHandler,
   MaterialIcons,
 } from "./types";
