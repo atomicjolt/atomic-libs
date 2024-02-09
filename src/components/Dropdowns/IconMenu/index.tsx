bf4499eb2fbadae041b62512e2c522ec7af1d351
Sean Collings <me@seancollings.dev>
2024-02-09 10:03:58 -0700

    IconMenu supports loading props

--- src/components/Dropdowns/IconMenu/index.tsx
+++ src/components/Dropdowns/IconMenu/index.tsx
@@ -2,7 +2,7 @@ import React, { useRef } from "react";
 import { MenuTriggerProps, useMenuTriggerState } from "react-stately";
 import { AriaMenuProps, Placement, useMenuTrigger } from "react-aria";
 import IconButton from "../../Buttons/IconButton";
-import { BaseProps, HasIcon } from "../../../types";
+import { BaseProps, HasIcon, LoadingProps } from "../../../types";
 import Popover from "../../Utility/Popover";
 import { Menu } from "../Menu";
 import { ButtonVariants } from "../../Buttons/Buttons.types";
@@ -10,7 +10,8 @@ import { ButtonVariants } from "../../Buttons/Buttons.types";
 export type IconMenuProps<T> = AriaMenuProps<T> &
   MenuTriggerProps &
   BaseProps &
-  HasIcon & {
+  HasIcon &
+  LoadingProps & {
     isDisabled?: boolean;
     buttonVariant?: ButtonVariants;
     placement?: Placement;
@@ -32,6 +33,9 @@ export default function IconMenu<T extends {}>(props: IconMenuProps<T>) {
     buttonVariant,
     placement = "bottom right",
     isDisabled,
+    isLoading,
+    loadingComplete,
+    loadingLabel,
   } = props;
 
   return (
@@ -43,6 +47,9 @@ export default function IconMenu<T extends {}>(props: IconMenuProps<T>) {
         ref={ref}
         className={className}
         isDisabled={isDisabled}
+        isLoading={isLoading}
+        loadingComplete={loadingComplete}
+        loadingLabel={loadingLabel}
         {...menuTriggerProps}
       />
       {state.isOpen && (
