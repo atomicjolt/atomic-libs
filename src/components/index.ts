6872354600cfe9cdb1c8102fdb52ec19f3c832fd
Sean Collings <sean.collings@atomicjolt.com>
2024-03-28 13:10:30 -0600

    Export MultiSelectProps

--- src/components/index.ts
+++ src/components/index.ts
@@ -104,3 +104,4 @@ export type { DatePickerProps } from "./Inputs/DateAndTimes/DatePicker";
 export type { ListBoxProps } from "./Atoms/ListBox";
 export type { SkeletonLoaderProps } from "./Loaders/SkeletonLoader";
 export type { DevelopmentErrorModalProps } from "./Development/DevelopmentErrorModal";
+export type { MultiSelectProps } from "./Dropdowns/MultiSelect";
