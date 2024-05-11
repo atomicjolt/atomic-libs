042806f0302a155e7cfda45c5b6826b249aa19fa
Sean Collings <sean.collings@atomicjolt.com>
2024-05-10 18:44:22 -0600

    Added import aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 // TODO: this is currently only used for storybook. Look into using vite for building atomic elements as well.
 
 export default defineConfig({
   plugins: [react()],
+
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+      "@sb": path.resolve(__dirname, ".storybook/utils"),
+    },
+  },
 });
