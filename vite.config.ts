43e7b7349428190511064a74414b5feb1839730c
Sean Collings <sean.collings@atomicjolt.com>
2024-05-14 12:26:50 -0600

    fixed dev storybook

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,9 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    exclude: ["storybook"],
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
