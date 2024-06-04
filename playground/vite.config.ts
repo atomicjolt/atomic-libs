cf0ec0b3e6a7e517aa4113fbf23fcf4b2f775036
Sean Collings <sean.collings@atomicjolt.com>
2024-06-04 11:32:56 -0600

    Updated More dependencies

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from "vite";
-import reactRefresh from "@vitejs/plugin-react-refresh";
+import react from "@vitejs/plugin-react";
 import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   root: "playground",
-  plugins: [reactRefresh()],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "../src"),
