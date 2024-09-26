bf4319688ab71f9cdcaaa49d173fd12a16e7b412
Sean Collings <sean.collings@atomicjolt.com>
2024-09-26 14:52:59 -0600

    Updated snapshots

--- playground/src/tabs/Aria.tsx
+++ playground/src/tabs/Aria.tsx
@@ -6,34 +6,11 @@ export default function Aria() {
 
   return (
     <div>
-      <h1>With total pages</h1>
-
-      <Pagination totalPages={10} page={page} onChange={setPage}>
+      <Pagination totalPages={10} page={page} onPageChange={setPage}>
         <Flex gap={8}>
           <Pagination.FirstPage />
           <Pagination.PrevPage />
           <Pagination.PageList>
-            {(page, state) => (
-              <Pagination.Page
-                page={page}
-                variant={page === state.page ? "primary" : "ghost"}
-              >
-                {page}
-              </Pagination.Page>
-            )}
-          </Pagination.PageList>
-          <Pagination.NextPage />
-          <Pagination.LastPage />
-        </Flex>
-      </Pagination>
-
-      <h1>Without total pages</h1>
-
-      <Pagination page={page} onChange={setPage}>
-        <Flex gap={8}>
-          <Pagination.FirstPage />
-          <Pagination.PrevPage />
-          <Pagination.PageList totalPages={10}>
             {(page, state) => (
               <Pagination.Page
                 page={page}
