fad57a690051ced782128c67ff5924d3602d2b46
Sean Collings <sean.collings@atomicjolt.com>
2024-11-15 10:40:05 -0700

    Remove build files for atomic-fuel

--- packages/atomic-fuel/libs/constants/network.d.ts
+++
@@ -1,8 +0,0 @@
-declare namespace _default {
-    let GET: string;
-    let POST: string;
-    let PUT: string;
-    let DEL: string;
-    let TIMEOUT: number;
-}
-export default _default;
--- packages/atomic-fuel/libs/decorators/modal.d.ts
+++
@@ -1,4 +0,0 @@
-export default function modalDecorator(Component: any, name: any): import("react-redux").ConnectedComponent<any, {
-    context?: import("react-redux/es/components/Context").ReactReduxContextInstance | undefined;
-    store?: import("redux").Store | undefined;
-}>;
