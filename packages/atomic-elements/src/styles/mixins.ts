3f1e3e08943f114e51ed7e4f9e4356092b997516
Sean Collings <sean.collings@atomicjolt.com>
2025-01-28 15:16:46 -0700

    chore(elements): CSS mixins are exported individually now

--- packages/atomic-elements/src/styles/mixins.ts
+++ packages/atomic-elements/src/styles/mixins.ts
@@ -1,6 +1,6 @@
 import { css, Interpolation } from "styled-components";
 
-const Sizing = css`
+export const Sizing = css`
   &.is-small {
     --size-x: var(--size-sm-x);
     --size-y: var(--size-sm-y);
@@ -27,100 +27,118 @@ const Sizing = css`
   }
 `;
 
-const mixins = {
-  Bold: css`
-    font-family: var(--font-family);
-    font-weight: var(--font-weight-bold);
-  `,
-  Regular: css`
-    font-family: var(--font-family);
-    font-weight: var(--font-weight-regular);
-  `,
-  FieldStatus: css`
-    &[data-required] {
-      label::after {
-        content: " *";
-      }
+export const Bold = css`
+  font-family: var(--font-family);
+  font-weight: var(--font-weight-bold);
+`;
+
+export const Regular = css`
+  font-family: var(--font-family);
+  font-weight: var(--font-weight-regular);
+`;
+
+export const FieldStatus = css`
+  &[data-required] {
+    label::after {
+      content: " *";
+    }
+  }
+
+  &[data-invalid] {
+    --input-outline: 1px solid var(--error-clr);
+  }
+
+  &[data-disabled] {
+    opacity: 0.5;
+  }
+
+  &[data-readonly] {
+    --input-border-clr: var(--neutral100);
+    --input-bg-clr: var(--neutral100);
+  }
+`;
+
+export const InputLike = css`
+  border-style: var(--input-border-style);
+  border-width: var(--input-border-width);
+  border-color: var(--input-border-clr);
+  border-radius: var(--input-border-radius);
+  min-height: var(--input-height);
+  padding: 0 var(--input-padding-horiz);
+  font-size: var(--input-font-size);
+  color: var(--input-text-clr);
+  background-color: var(--input-bg-clr);
+  text-align: left;
+
+  i {
+    color: var(--input-icon-clr);
+  }
+
+  &:focus,
+  &:focus-within {
+    --input-border-clr: var(--outline-clr-primary);
+    outline: var(--input-outline);
+  }
+`;
+
+export const SizingX = css`
+  ${Sizing}
+  width: var(--size-x);
+`;
+
+export const SizingY = css`
+  ${Sizing}
+  height: var(--size-y);
+`;
+
+export const FocusVisible = (oultineOffset: number = 0) => css`
+  &:focus-visible {
+    outline: none;
+  }
+
+  &[data-focus-visible="true"] {
+    outline: var(--outline);
+    outline-offset: ${oultineOffset}px;
+  }
+`;
+
+export const ToggleInputLike = (element: Interpolation<any>) => {
+  return css`
+    display: block;
+
+    :is(input):focus-visible ~ ${element}:before {
+      outline: var(--outline);
+      outline-offset: 2px;
     }
 
-    &[data-invalid] {
-      --input-outline: 1px solid var(--error-clr);
-    }
-
-    &[data-disabled] {
-      opacity: 0.5;
-    }
-
-    &[data-readonly] {
-      --input-border-clr: var(--neutral100);
-      --input-bg-clr: var(--neutral100);
-    }
-  `,
-  InputLike: css`
-    border-style: var(--input-border-style);
-    border-width: var(--input-border-width);
-    border-color: var(--input-border-clr);
-    border-radius: var(--input-border-radius);
-    min-height: var(--input-height);
-    padding: 0 var(--input-padding-horiz);
-    font-size: var(--input-font-size);
-    color: var(--input-text-clr);
-    background-color: var(--input-bg-clr);
-    text-align: left;
-
-    i {
-      color: var(--input-icon-clr);
-    }
-
-    &:focus,
-    &:focus-within {
-      --input-border-clr: var(--outline-clr-primary);
-      outline: var(--input-outline);
-    }
-  `,
-  Sizing,
-  SizingX: css`
-    ${Sizing}
-    width: var(--size-x);
-  `,
-  SizingY: css`
-    ${Sizing}
-    height: var(--size-y);
-  `,
-  FocusVisible: (oultineOffset: number = 0) => css`
-    &:focus-visible {
+    :is(input):focus:not(:focus-visible) {
       outline: none;
     }
 
-    &[data-focus-visible="true"] {
-      outline: var(--outline);
-      outline-offset: ${oultineOffset}px;
+    &[data-invalid] span::before {
+      border-color: var(--error700);
     }
-  `,
-  ToggleInputLike: (element: Interpolation<any>) => {
-    return css`
-      display: block;
+  `;
+};
 
-      :is(input):focus-visible ~ ${element}:before {
-        outline: var(--outline);
-        outline-offset: 2px;
-      }
+export const Border = (prefix: string, { style = "solid" } = {}) => css`
+  border-width: var(--${prefix}-border-width);
+  border-color: var(--${prefix}-border-clr);
+  border-style: var(--${prefix}-border-style, ${style});
+  border-radius: var(--${prefix}-border-radius, 0);
+`;
 
-      :is(input):focus:not(:focus-visible) {
-        outline: none;
-      }
-
-      &[data-invalid] span::before {
-        border-color: var(--error700);
-      }
-    `;
-  },
-  Border: (prefix: string, { style = "solid" } = {}) => css`
-    border-width: var(--${prefix}-border-width);
-    border-color: var(--${prefix}-border-clr);
-    border-style: var(--${prefix}-border-style, ${style});
-    border-radius: var(--${prefix}-border-radius, 0);
-  `,
+const mixins = {
+  Bold,
+  Regular,
+  FieldStatus,
+  InputLike,
+  Sizing,
+  SizingX,
+  SizingY,
+  FocusVisible,
+  ToggleInputLike,
+  Border,
 };
 
 export default mixins;
