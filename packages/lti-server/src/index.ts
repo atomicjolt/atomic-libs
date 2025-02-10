5431ff2ea669fe58c190b5b150c3d354e657c0c9
Justin Ball <justinball@gmail.com>
2025-02-10 13:42:46 -0700

    Change lti-server exports

--- packages/lti-server/src/index.ts
+++ packages/lti-server/src/index.ts
@@ -1,22 +1,16 @@
 export * from './types';
-export * as LtiPlatform from './libs/platform_storage';
-export * as LtiValidation from './libs/lti_validation';
-export * as LtiOidc from './libs/oidc';
-export * as LtiToolConfiguration from './libs/tool_configuration';
-export * as LtiJwt from './libs/jwt';
-export * as LtiJwks from './libs/jwks';
-export * as LtiScores from './libs/scores';
-export * as LtiResults from './libs/results';
-export * as LtiLineItems from './libs/line_items';
-export * as LtiNamesAndRoles from './libs/names_and_roles';
-export * as LtiPlaformStorage from './libs/platform_storage';
+export * from './libs/constants';
+export * from './libs/platform_storage';
+export * from './libs/lti_validation';
+export * from './libs/oidc';
+export * from './libs/tool_configuration';
+export * from './libs/jwt';
+export * from './libs/jwks';
+export * from './libs/scores';
+export * from './libs/results';
+export * from './libs/line_items';
+export * from './libs/names_and_roles';
+export * from './libs/client_credentials';
+export * from './libs/link_header';
 
-export * as ClientCredentials from './libs/client_credentials';
-export * as LinkHeader from './libs/link_header';
-
-export {
-  OPEN_ID_COOKIE_PREFIX,
-  OPEN_ID_STORAGE_COOKIE,
-  ALLOWED_LAUNCH_TIME,
-} from './libs/constants';
-export { TEST_ID_TOKEN, genJwt } from './tests/helper';
+export * from './tests/helper';
