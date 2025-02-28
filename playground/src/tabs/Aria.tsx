f3c717b03ccac6e16ed9463845101f7f7828b063
Sean Collings <sean.collings@atomicjolt.com>
2025-02-28 11:03:57 -0700

    chore: added changeset

--- playground/src/tabs/Aria.tsx
+++ playground/src/tabs/Aria.tsx
@@ -1,43 +1,18 @@
 import React from "react";
-import { Text, Card } from "../elements";
+import { View, Text, Flex } from "@atomicjolt/atomic-elements";
 
 export default function Aria() {
   return (
-    <Card>
-      {/* <Card.Stack>
-        <Card.Columns>
-          <Card.Panel>
-            <Text $size="3" $weight="bold">
-              Panel 1
-            </Text>
-          </Card.Panel>
-
-          <Card.Panel>
-            <Text $size="3" $weight="bold">
-              Panel 3
-            </Text>
-          </Card.Panel>
-        </Card.Columns>
-
-        <Card.Panel>
-          <Text $size="3">Panel 2</Text>
-        </Card.Panel>
-      </Card.Stack> */}
-
-      <Card.Grid $columns="repeat(3, 200px)" $rows="2">
-        <Card.Panel $height="200px">
-          <Text $size="3">Panel 1</Text>
-        </Card.Panel>
-        <Card.Panel>
-          <Text $size="3">Panel 2</Text>
-        </Card.Panel>
-        <Card.Panel>
-          <Text $size="3">Panel 3</Text>
-        </Card.Panel>
-        <Card.Panel>
-          <Text $size="3">Panel 4</Text>
-        </Card.Panel>
-      </Card.Grid>
-    </Card>
+    <Flex $direction="column" $gap="3">
+      <Text $size="1">The quick brown fox jumps over the lazy dog.</Text>
+      <Text $size="2">The quick brown fox jumps over the lazy dog.</Text>
+      <Text $size="3">The quick brown fox jumps over the lazy dog.</Text>
+      <Text $size="4">The quick brown fox jumps over the lazy dog.</Text>
+      <Text $size="5">The quick brown fox jumps over the lazy dog.</Text>
+      <Text $size="6">The quick brown fox jumps over the lazy dog.</Text>
+      <Text $size="7">The quick brown fox jumps over the lazy dog.</Text>
+      <Text $size="8">The quick brown fox jumps over the lazy dog.</Text>
+      <Text $size="9">The quick brown fox jumps over the lazy dog.</Text>
+    </Flex>
   );
 }
