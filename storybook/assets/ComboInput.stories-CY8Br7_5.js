740ff0c3c017cc3ce39e601cedbae9ef16940128
seanrcollings <seanrcollings@users.noreply.github.com>
2025-04-01 22:19:19 +0000

    deploy: acf37f9bb9ca91514a27200236304d8a9de6c747

---
+++ storybook/assets/ComboInput.stories-CY8Br7_5.js
@@ -0,0 +1,24 @@
+import{j as r}from"./jsx-runtime-D_zvdyIk.js";import{C as n}from"./ComboInput.component-CjIXbHRN.js";import{M as b}from"./MaterialIcon.component-w-Hb6GVM.js";import{I as p}from"./Input.component-Cuh-x2Kc.js";import{I as x}from"./IconButton.component-P1ChaKux.js";import{N as m}from"./NumberField.component-DIfutQMi.js";import{G as j}from"./Group.component-DclGvoAD.js";import"./index-DmM0KDA7.js";import"./ComboInput.styles-BLZOd1-l.js";import"./mixins-CgZ-Dp8B.js";import"./styled-components.browser.esm-B0S50oBn.js";import"./index-CqJC2f1M.js";import"./useObjectRef-BO7mrmvR.js";import"./SSRProvider-nIDwi1ZO.js";import"./ComboInput.context-Di7U2yCG.js";import"./useRenderProps-BzxgHqRb.js";import"./index-CrEZfznw.js";import"./Icons.styles-D2L4nNgu.js";import"./filterDOMProps-CeZl_uWj.js";import"./useFocusable-B7cTVP20.js";import"./Input.context-BTEI3GdY.js";import"./Button.component-BObpPVxs.js";import"./useFocusRing-4PdL76fJ.js";import"./useFocusWithin-CrVHK3Cv.js";import"./usePress-Cxy0og0d.js";import"./index-CnHy7ftH.js";import"./index-nLeaPAJ8.js";import"./useButton-DlM8S39G.js";import"./useLink-CqmZ9M1P.js";import"./SpinnerLoader.component-DGq62Hsy.js";import"./Loader.component-D2Ngq9hJ.js";import"./Flex.component-DSJ8Mwfi.js";import"./layout-C5ppTKTq.js";import"./spacing-Bd-CIscW.js";import"./utils-DqmNl-Il.js";import"./Checkmark.component-CdBGiybJ.js";import"./Spinner.component-fO3c5uZz.js";import"./ProgressCircle.component-dfZhFUDA.js";import"./useLabel-B1sX0IQZ.js";import"./useLabels-D0NjiLiF.js";import"./useNumberFormatter-CH8tnwcW.js";import"./context-BF7eBGFA.js";import"./NumberFormatter-DNR9MAW-.js";import"./colors-x_YFGAop.js";import"./useFormReset-DvzwwQm6.js";import"./useFormValidationState-CKAS44yz.js";import"./useEvent-Qm4q9YOK.js";import"./useTextField-B3HDaRNl.js";import"./useControlledState-sJfMLWrs.js";import"./useField-BhG43OVF.js";import"./useFormValidation-CsrNRCZi.js";import"./useLocalizedStringFormatter-BJB32GAh.js";import"./useSpinButton-DS2l_r0R.js";import"./LiveAnnouncer-CeCcBDbP.js";import"./Field.styles-B11u_h4j.js";import"./Provider-op_UCnZE.js";import"./Label.context-8YwVGsEO.js";import"./Message.context-4_rHhebz.js";import"./ErrorMessage.context-D-RNFvKn.js";const Br={title:"Fields/ComboInput",component:n,parameters:{layout:"centered"},argTypes:{padding:{control:"multi-select",options:["left","right"]}}},t={args:{children:[r.jsx(b,{icon:"search"},"icon"),r.jsx(p,{placeholder:"Search"},"actual-input")]}},o={render:e=>r.jsx(n,{...e}),args:{padding:"left",children:[r.jsx(p,{},"input"),r.jsx("span",{children:"/10"},"total"),r.jsx(x,{icon:"search",variant:"ghost"},"icon")]}},i={render:e=>r.jsx(m,{children:r.jsxs(n,{...e,children:[r.jsx(p,{}),r.jsxs(j,{gap:0,children:[r.jsx(m.DecrementButton,{icon:"remove",variant:"ghost",size:"small"}),r.jsx(m.IncrementButton,{icon:"add",variant:"ghost",size:"small"})]})]})}),args:{padding:"left"}};var a,s,c;t.parameters={...t.parameters,docs:{...(a=t.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {
+    children: [<MaterialIcon icon="search" key="icon" />, <Input placeholder="Search" key="actual-input" />]
+  }
+}`,...(c=(s=t.parameters)==null?void 0:s.docs)==null?void 0:c.source}}};var u,d,l;o.parameters={...o.parameters,docs:{...(u=o.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  render: args => <ComboInput {...args}></ComboInput>,
+  args: {
+    padding: "left",
+    children: [<Input key="input" />, <span key="total">/10</span>, <IconButton icon="search" variant="ghost" key="icon" />]
+  }
+}`,...(l=(d=o.parameters)==null?void 0:d.docs)==null?void 0:l.source}}};var h,g,I;i.parameters={...i.parameters,docs:{...(h=i.parameters)==null?void 0:h.docs,source:{originalSource:`{
+  render: args => <NumberField>
+      <ComboInput {...args}>
+        <FieldInput />
+        <Group gap={0}>
+          <NumberField.DecrementButton icon="remove" variant="ghost" size="small" />
+          <NumberField.IncrementButton icon="add" variant="ghost" size="small" />
+        </Group>
+      </ComboInput>
+    </NumberField>,
+  args: {
+    padding: "left"
+  }
+}`,...(I=(g=i.parameters)==null?void 0:g.docs)==null?void 0:I.source}}};const Cr=["Primary","NumberInputWithSearch","NumberFieldWithButtons"];export{i as NumberFieldWithButtons,o as NumberInputWithSearch,t as Primary,Cr as __namedExportsOrder,Br as default};
---
+++ storybook/assets/IconMenu.stories-B8hIWy3N.js
@@ -0,0 +1,39 @@
+import{j as e}from"./jsx-runtime-D_zvdyIk.js";import{g as j}from"./cssprops-BO85xpNm.js";import"./index-Do_mbUnv.js";import"./index-BDzxygkP.js";import"./index-D4IwYvU9.js";import{I as t,S as b}from"./Collection-CfmUX7eJ.js";import"./index-SU87TpAq.js";import{I as f}from"./IconMenu.component-yRGJF-0C.js";import{M as n}from"./MaterialIcon.component-w-Hb6GVM.js";import"./CollectionBuilder-BOiBnYCj.js";import"./index-DmM0KDA7.js";import"./index-CnHy7ftH.js";import"./index-nLeaPAJ8.js";import"./useFocusable-B7cTVP20.js";import"./useObjectRef-BO7mrmvR.js";import"./SSRProvider-nIDwi1ZO.js";import"./MenuTrigger-Cl8qP7UA.js";import"./useMenuTrigger-DLEtYr5F.js";import"./useSelectableItem-CQFuthIx.js";import"./usePress-Cxy0og0d.js";import"./useEvent-Qm4q9YOK.js";import"./scrollIntoView-DuDju04b.js";import"./isScrollable-BSEN4xi5.js";import"./FocusScope-CLRP0VYY.js";import"./context-BF7eBGFA.js";import"./useDescription-CP-qk7_s.js";import"./useLocalizedStringFormatter-BJB32GAh.js";import"./useOverlayTrigger-BaVZjogA.js";import"./useCloseOnScroll-CkGkhb-Y.js";import"./PressResponder-D-PDHmhd.js";import"./Provider-op_UCnZE.js";import"./Popover.context-CkZWeej5.js";import"./index-CqJC2f1M.js";import"./Menu.component-D9F0mhW0.js";import"./index-CrEZfznw.js";import"./filterDOMProps-CeZl_uWj.js";import"./useSelectableList-CerkSCCR.js";import"./DOMLayoutDelegate-NFyoR3Uz.js";import"./useCollator-DZEoqIyA.js";import"./getItemCount-CHdBXIUM.js";import"./useHover-BMz_rS5A.js";import"./useForwardedRef-BWfLegUW.js";import"./useCollectionRenderer-Dwym1jfR.js";import"./useControlledState-sJfMLWrs.js";import"./useRenderProps-BzxgHqRb.js";import"./useFocusRing-4PdL76fJ.js";import"./useFocusWithin-CrVHK3Cv.js";import"./styled-components.browser.esm-B0S50oBn.js";import"./mixins-CgZ-Dp8B.js";import"./Divider.component-Cp_2XO01.js";import"./utils-DqmNl-Il.js";import"./context-D5CkyXRW.js";import"./getScrollParent-BL0kfGii.js";import"./useOverlayTriggerState-3VWy5d8s.js";import"./IconButton.component-P1ChaKux.js";import"./Button.component-BObpPVxs.js";import"./useButton-DlM8S39G.js";import"./useLink-CqmZ9M1P.js";import"./SpinnerLoader.component-DGq62Hsy.js";import"./Loader.component-D2Ngq9hJ.js";import"./Flex.component-DSJ8Mwfi.js";import"./layout-C5ppTKTq.js";import"./spacing-Bd-CIscW.js";import"./Checkmark.component-CdBGiybJ.js";import"./Spinner.component-fO3c5uZz.js";import"./ProgressCircle.component-dfZhFUDA.js";import"./useLabel-B1sX0IQZ.js";import"./useLabels-D0NjiLiF.js";import"./useNumberFormatter-CH8tnwcW.js";import"./NumberFormatter-DNR9MAW-.js";import"./colors-x_YFGAop.js";import"./Popover.component-DX0xQw-g.js";import"./VisuallyHidden-BPDCoBu-.js";import"./useOverlayPosition-BbD5l9N4.js";import"./Icons.styles-D2L4nNgu.js";const Ge={title:"Dropdowns/Menu/IconMenu",component:f,parameters:{layout:"centered",cssprops:j("Dropdown","Menu","Button")},argTypes:{buttonVariant:{control:"select",options:["primary","secondary","error","success","inverted","content"]},iconVariant:{control:"select",options:["default","outlined","round","sharp","two-tone"]},children:{control:!1},onClose:{description:"Callback for when the menu is closed.",table:{category:"Events"}},onOpenChange:{description:"Callback for when the menu is opened or closed.",table:{category:"Events"}},isOpen:{control:"boolean",description:"Control the open state of the menu externally."},defaultOpen:{control:"boolean",description:"Whether the menu is open by default."},disabledKeys:{control:"multi-select",options:["item1","item2","item3"],description:"Keys of items that should be disabled."}}},r={args:{icon:"more_vert",children:[e.jsx(t,{id:"item1",onAction:()=>alert("Item 1"),children:"Item 1"},"item1"),e.jsx(t,{id:"item2",onAction:()=>alert("Item 2"),children:"Item 2"},"item2"),e.jsx(t,{id:"item3",onAction:()=>alert("Item 3"),children:"Item 3"},"item3")]}},o={args:{...r.args,children:[e.jsx(t,{children:"Item 1"},"item1"),e.jsx(t,{children:"Item 2"},"item2"),e.jsxs(b,{children:[e.jsx(t,{children:"Item 3"},"item3"),e.jsx(t,{children:"Item 4"},"item4")]},"section")]}},i={args:{...r.args,children:[e.jsxs(t,{children:[e.jsx(n,{icon:"edit"}),"Edit"]},"item1"),e.jsxs(t,{children:[e.jsx(n,{icon:"delete"}),"Delete"]},"item2"),e.jsxs(t,{children:[e.jsx(n,{icon:"archive"}),"Archive"]},"item3")]}},m={args:{...r.args,disabledKeys:["item2"]}};var s,p,a;r.parameters={...r.parameters,docs:{...(s=r.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    icon: "more_vert",
+    children: [<Item id="item1" key="item1" onAction={() => alert("Item 1")}>
+        Item 1
+      </Item>, <Item id="item2" key="item2" onAction={() => alert("Item 2")}>
+        Item 2
+      </Item>, <Item id="item3" key="item3" onAction={() => alert("Item 3")}>
+        Item 3
+      </Item>]
+  }
+}`,...(a=(p=r.parameters)==null?void 0:p.docs)==null?void 0:a.source}}};var c,l,d;o.parameters={...o.parameters,docs:{...(c=o.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    ...Primary.args,
+    children: [<Item key="item1">Item 1</Item>, <Item key="item2">Item 2</Item>, <Section key="section">
+        <Item key="item3">Item 3</Item>
+        <Item key="item4">Item 4</Item>
+      </Section>]
+  }
+}`,...(d=(l=o.parameters)==null?void 0:l.docs)==null?void 0:d.source}}};var I,h,u;i.parameters={...i.parameters,docs:{...(I=i.parameters)==null?void 0:I.docs,source:{originalSource:`{
+  args: {
+    ...Primary.args,
+    children: [<Item key="item1">
+        <MaterialIcon icon="edit" />
+        Edit
+      </Item>, <Item key="item2">
+        <MaterialIcon icon="delete" />
+        Delete
+      </Item>, <Item key="item3">
+        <MaterialIcon icon="archive" />
+        Archive
+      </Item>]
+  }
+}`,...(u=(h=i.parameters)==null?void 0:h.docs)==null?void 0:u.source}}};var y,g,x;m.parameters={...m.parameters,docs:{...(y=m.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  args: {
+    ...Primary.args,
+    disabledKeys: ["item2"]
+  }
+}`,...(x=(g=m.parameters)==null?void 0:g.docs)==null?void 0:x.source}}};const He=["Primary","WithSections","WithIcons","DisabledItems"];export{m as DisabledItems,r as Primary,i as WithIcons,o as WithSections,He as __namedExportsOrder,Ge as default};
