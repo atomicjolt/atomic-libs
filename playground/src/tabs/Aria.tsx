16372dd8fd86e3ee773a7398b0d92cf3b42fac00
Sean Collings <sean.collings@atomicjolt.com>
2025-04-01 16:14:20 -0600

    fix: button supprots dynamic classnames correctly

--- playground/src/tabs/Aria.tsx
+++ playground/src/tabs/Aria.tsx
@@ -1,18 +1,14 @@
 import React from "react";
-import { View, Text, Flex } from "@atomicjolt/atomic-elements";
+import { View, Text, Flex, IconButton, TextInput, Button } from "../elements";
 
 export default function Aria() {
   return (
-    <Flex $direction="column" $gap="3">
-      <Text $size="1">The quick brown fox jumps over the lazy dog.</Text>
-      <Text $size="2">The quick brown fox jumps over the lazy dog.</Text>
-      <Text $size="3">The quick brown fox jumps over the lazy dog.</Text>
-      <Text $size="4">The quick brown fox jumps over the lazy dog.</Text>
-      <Text $size="5">The quick brown fox jumps over the lazy dog.</Text>
-      <Text $size="6">The quick brown fox jumps over the lazy dog.</Text>
-      <Text $size="7">The quick brown fox jumps over the lazy dog.</Text>
-      <Text $size="8">The quick brown fox jumps over the lazy dog.</Text>
-      <Text $size="9">The quick brown fox jumps over the lazy dog.</Text>
-    </Flex>
+    <>
+      <IconButton className={{ test: true }} icon="import_contacts" />
+
+      <Button className={{ test: true }}>Test</Button>
+
+      <TextInput className={{ test: true }} label="Name" />
+    </>
   );
 }
