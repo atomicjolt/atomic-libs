5e253d2e4bf89a099d4f56cc6d5d53892948fae6
seanrcollings <seanrcollings@users.noreply.github.com>
2025-04-29 16:12:12 +0000

    deploy: d9c8ad443329e02ec2833530ff8b39eb44605ecd

---
+++ storybook/assets/IconButton.stories-BFZjRIor.js
@@ -0,0 +1,18 @@
+import s from"./Button.stories-DmDKDL-o.js";import{g as n}from"./cssprops-BO85xpNm.js";import{I as l}from"./IconButton.component-U5atzCO9.js";import"./jsx-runtime-D_zvdyIk.js";import"./helpers-C1PCfQ2p.js";import"./styled-components.browser.esm-Y4FpNNpy.js";import"./index-BCtMShv3.js";import"./spacing-Bd-CIscW.js";import"./scale-CqCDTNu0.js";import"./Collection-DqSZYP0m.js";import"./CollectionBuilder-D7AJqdfd.js";import"./index-BqCLlfdy.js";import"./index-D-fs5e6L.js";import"./useFocusable-DacP9xvE.js";import"./useObjectRef-D2RG7rRi.js";import"./SSRProvider-DyiXDq2k.js";import"./Button.component-W0MMZ58g.js";import"./useFocusRing-CLjBjOxD.js";import"./useFocusWithin-BshFo8_g.js";import"./usePress-B9aiz4Kw.js";import"./useRenderProps-CP918x9p.js";import"./index-EJ0-2BeM.js";import"./useButton-nVeIkIqk.js";import"./filterDOMProps-CeZl_uWj.js";import"./useLink-FIfyncSu.js";import"./index-CsiUWFDz.js";import"./mixins-XviowZ_6.js";import"./Button.context-BxdzJgC_.js";import"./SpinnerLoader.component-_s3mi7W0.js";import"./Loader.component-DMkqasMO.js";import"./Flex.component-rrKmR1Yr.js";import"./layout-C5ppTKTq.js";import"./utils-DqmNl-Il.js";import"./Checkmark.component-Bisr1A6O.js";import"./Spinner.component-C44VOTWe.js";import"./ProgressCircle.component-_TQleApR.js";import"./number-nHrFdSb-.js";import"./useLabel-DDcndmXW.js";import"./useLabels-B8dXFA8d.js";import"./useNumberFormatter-BHOsbS6G.js";import"./context-z6pb9OkM.js";import"./NumberFormatter-DNR9MAW-.js";import"./colors-x_YFGAop.js";import"./MaterialIcon.component-BeI1h3Qw.js";import"./Icons.styles-D-JqKhr6.js";const to={title:"Buttons/IconButton",component:l,parameters:{layout:"centered",cssprops:n("Button")},argTypes:{...s.argTypes}},o={args:{isDisabled:!1,icon:"more_vert","aria-label":"More options",isLoading:!1,iconVariant:"default",variant:"border"}},r={args:{icon:"more_vert","aria-label":"More options",isLoading:!0,loadingLabel:"Loading...",loadingComplete:!1}};var t,i,a;o.parameters={...o.parameters,docs:{...(t=o.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    isDisabled: false,
+    icon: "more_vert",
+    "aria-label": "More options",
+    isLoading: false,
+    iconVariant: "default",
+    variant: "border"
+  }
+}`,...(a=(i=o.parameters)==null?void 0:i.docs)==null?void 0:a.source}}};var e,m,p;r.parameters={...r.parameters,docs:{...(e=r.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  args: {
+    icon: "more_vert",
+    "aria-label": "More options",
+    isLoading: true,
+    loadingLabel: "Loading...",
+    loadingComplete: false
+  }
+}`,...(p=(m=r.parameters)==null?void 0:m.docs)==null?void 0:p.source}}};const io=["Primary","isLoading"];export{o as Primary,io as __namedExportsOrder,to as default,r as isLoading};
