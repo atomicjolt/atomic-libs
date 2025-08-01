d0de9d19666ab5e3c2c99ceecc48c907cffdd982
seanrcollings <seanrcollings@users.noreply.github.com>
2025-08-01 18:53:23 +0000

    deploy: cb25093333062c5c3b68e02ab9c40595147f3c92

---
+++ storybook/assets/DisclosureGroup.stories-Dqx-2_Sf.js
@@ -0,0 +1,34 @@
+import{j as e}from"./jsx-runtime-D_zvdyIk.js";import{R as n}from"./helpers-C1PCfQ2p.js";import{fn as p}from"./index-BgJgh-x_.js";import{a,D as i}from"./Disclosure.component-C7JWbaEq.js";import{F as l}from"./Flex.component-BTH7Jim3.js";import"./styled-components.browser.esm-Y4FpNNpy.js";import"./index-BCtMShv3.js";import"./spacing-Bd-CIscW.js";import"./scale-CqCDTNu0.js";import"./Collection-DqSZYP0m.js";import"./CollectionBuilder-D7AJqdfd.js";import"./index-BqCLlfdy.js";import"./index-D-fs5e6L.js";import"./useFocusable-DacP9xvE.js";import"./useObjectRef-D2RG7rRi.js";import"./SSRProvider-DyiXDq2k.js";import"./useEvent-CeKNPFU-.js";import"./index-CsiUWFDz.js";import"./Button.component-82vpXv-0.js";import"./useFocusRing-88kjlAJd.js";import"./useFocusWithin-AgzTPs3-.js";import"./usePress-CzUfyTu7.js";import"./useRenderProps-CP918x9p.js";import"./index-EJ0-2BeM.js";import"./useButton-DYDUZLxM.js";import"./filterDOMProps-CeZl_uWj.js";import"./useLink-D8UViwUR.js";import"./mixins-XviowZ_6.js";import"./Button.context-BxdzJgC_.js";import"./SpinnerLoader.component-5_47Yvoq.js";import"./Loader.component-DCLKElU7.js";import"./Checkmark.component-Bisr1A6O.js";import"./Spinner.component-C59bpdXu.js";import"./ProgressCircle.component-gsiCSqhQ.js";import"./number-nHrFdSb-.js";import"./useLabel-DDcndmXW.js";import"./useLabels-B8dXFA8d.js";import"./useNumberFormatter-BHOsbS6G.js";import"./context-z6pb9OkM.js";import"./NumberFormatter-DNR9MAW-.js";import"./colors-x_YFGAop.js";import"./Provider-op_UCnZE.js";import"./useControlledState-vzCMHZvt.js";import"./MaterialIcon.component-iQbEbiKP.js";import"./Icons.styles-D-JqKhr6.js";import"./layout-Dd7m2B0D.js";import"./utils-DqmNl-Il.js";const re={title:"Content/Disclosure/DisclosureGroup",component:a,argTypes:{...n,allowsMultipleExpanded:{type:"boolean",description:"Allow multiple panels to be expanded at once"},defaultExpandedKeys:{description:"Keys of the panels that are expanded by default (uncontrolled)"},expandedKeys:{description:"Keys of the panels that are expanded (controlled)"},onExpandedChange:{table:{category:"Events"}},isDisabled:{type:"boolean",description:"Disable all panels"}}},o={args:{allowsMultipleExpanded:!1,onExpandedChange:p(),children:[e.jsxs(l,{$direction:"column",$gap:"10px",children:[e.jsxs(i,{id:"d1",children:[e.jsx(i.Trigger,{children:"First Disclosure"}),e.jsx(i.Panel,{children:e.jsx("p",{style:{fontSize:"25px"},children:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos aliquid consequuntur expedita ratione sed ab fugiat laudantium. Porro expedita, voluptate aspernatur consequatur inventore corporis odio voluptatem quod, corrupti, iste tempora."})})]},"d1"),e.jsxs(i,{id:"d2",children:[e.jsx(i.Trigger,{children:"Second Disclosure"}),e.jsx(i.Panel,{children:e.jsx("p",{style:{fontSize:"25px"},children:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio nemo expedita officia quod, nihil molestiae voluptate vel laudantium facilis culpa debitis labore obcaecati temporibus ut repellendus, ab similique harum accusantium."})})]},"d2")]},"flex")]}};var r,t,s;o.parameters={...o.parameters,docs:{...(r=o.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {
+    allowsMultipleExpanded: false,
+    onExpandedChange: fn(),
+    children: [<Flex key="flex" $direction="column" $gap="10px">
+        <Disclosure key="d1" id="d1">
+          <Disclosure.Trigger>First Disclosure</Disclosure.Trigger>
+          <Disclosure.Panel>
+            <p style={{
+            fontSize: "25px"
+          }}>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos
+              aliquid consequuntur expedita ratione sed ab fugiat laudantium.
+              Porro expedita, voluptate aspernatur consequatur inventore
+              corporis odio voluptatem quod, corrupti, iste tempora.
+            </p>
+          </Disclosure.Panel>
+        </Disclosure>
+        <Disclosure key="d2" id="d2">
+          <Disclosure.Trigger>Second Disclosure</Disclosure.Trigger>
+          <Disclosure.Panel>
+            <p style={{
+            fontSize: "25px"
+          }}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio
+              nemo expedita officia quod, nihil molestiae voluptate vel
+              laudantium facilis culpa debitis labore obcaecati temporibus ut
+              repellendus, ab similique harum accusantium.
+            </p>
+          </Disclosure.Panel>
+        </Disclosure>
+      </Flex>]
+  }
+}`,...(s=(t=o.parameters)==null?void 0:t.docs)==null?void 0:s.source}}};const te=["Primary"];export{o as Primary,te as __namedExportsOrder,re as default};
