d9789bbcecfccd3460c7c08953d9c7fa0a067ac3
Sean Collings <sean.collings@atomicjolt.com>
2026-06-18 13:18:32 -0600

    fix type exports

--- packages/atomic-elements/rollup.config.js
+++ packages/atomic-elements/rollup.config.js
@@ -24,6 +24,7 @@ const formatEntryPoint = (format) => ({
       tsconfig: "./tsconfig.build.json",
       compilerOptions: {
         baseUrl: "./",
+        rootDir: "./src",
         outDir: `dist/${format}`,
         declarationDir: `dist/${format}/types`,
         plugins: [
