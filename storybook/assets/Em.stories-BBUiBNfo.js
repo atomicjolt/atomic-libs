6c196eeafb37b36ea2e82bcf54231593fabb339d
seanrcollings <seanrcollings@users.noreply.github.com>
2026-08-24 20:57:55 +0000

    deploy: 02d5f510cc18db6aac7e61f9e25e13f44dddab32

---
+++ storybook/assets/Em.stories-BBUiBNfo.js
@@ -0,0 +1,12 @@
+import{j as a}from"./jsx-runtime-D_zvdyIk.js";import{T as f}from"./index-DQ-tAGWh.js";import{C as y,i as g,R as h}from"./helpers-B3wxvxjs.js";import{r as x}from"./index-BCtMShv3.js";import{c as E,u as T}from"./index-C_lK3xe_.js";import{u as _}from"./useRenderProps-CP918x9p.js";import{g as C}from"./styled-components.browser.esm-DC3GK9Rn.js";import{t as N}from"./colors-x_YFGAop.js";import{m as R}from"./layout-Dd7m2B0D.js";import"./typography-B_qJ0BtB.js";import"./scale-CqCDTNu0.js";import"./spacing-Bd-CIscW.js";import"./Collection-DwMxgycI.js";import"./CollectionBuilder-L_wKxqRd.js";import"./index-q6RvvsFA.js";import"./index-D-fs5e6L.js";import"./useFocusable-B7HbK8LX.js";import"./useObjectRef-B4hnndUj.js";import"./SSRProvider-DyiXDq2k.js";import"./index-EJ0-2BeM.js";import"./utils-DqmNl-Il.js";const j=E(),P=C.em`
+  ${R}
+  ${N}
+`,e=x.forwardRef(function(s,o){[s,o]=T(j,s,o);const{className:c,style:d,children:l,...u}=s,m=_({componentClassName:"aje-strong",className:c,style:d,children:l});return a.jsx(P,{ref:o,...m,...u,children:m.children})});e.displayName="Em";try{e.displayName="Em",e.__docgenInfo={description:"",displayName:"Em",props:{className:{defaultValue:null,description:"",name:"className",required:!1,type:{name:"RenderClassName<never>"}},style:{defaultValue:null,description:"",name:"style",required:!1,type:{name:"RenderStyle<never> | undefined"}}}}}catch{}const L={title:"Typography/Em",component:e,argTypes:{...h,...g,...y}},t={render:r=>a.jsxs(f,{$size:"3",children:["The most important step a man can take; it's not the first one, is it?"," ",a.jsx(e,{...r,children:r.children})]}),args:{children:"It's the next one."}};var n,i,p;t.parameters={...t.parameters,docs:{...(n=t.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  render: args => <Text $size="3">
+      The most important step a man can take; it's not the first one, is it?{" "}
+      <Em {...args}>{args.children}</Em>
+    </Text>,
+  args: {
+    children: "It's the next one."
+  }
+}`,...(p=(i=t.parameters)==null?void 0:i.docs)==null?void 0:p.source}}};const Q=["Primary"];export{t as Primary,Q as __namedExportsOrder,L as default};
---
+++ storybook/assets/Select.stories-CCHyfvIH.js
@@ -0,0 +1,60 @@
+import{j as e}from"./jsx-runtime-D_zvdyIk.js";import{I}from"./helpers-B3wxvxjs.js";import{R as _}from"./index-BCtMShv3.js";import{u as R}from"./util-DYWyeGCs.js";import{g as b}from"./styled-components.browser.esm-DC3GK9Rn.js";import{m as c}from"./mixins-CcgEHb9c.js";import{u as N}from"./useRenderProps-CP918x9p.js";import{L as w}from"./Label.component-PV5tL6Vm.js";import{M as z}from"./Message.component-BkeRZygz.js";import{E as j}from"./ErrorMessage.component-B_60Itgy.js";import"./spacing-Bd-CIscW.js";import"./scale-CqCDTNu0.js";import"./Collection-DwMxgycI.js";import"./CollectionBuilder-L_wKxqRd.js";import"./index-q6RvvsFA.js";import"./index-D-fs5e6L.js";import"./useFocusable-B7HbK8LX.js";import"./useObjectRef-B4hnndUj.js";import"./SSRProvider-DyiXDq2k.js";import"./index-EJ0-2BeM.js";import"./index-C_lK3xe_.js";import"./Label.context-Bxyvy4yx.js";import"./Message.context-CzffOAUK.js";import"./ErrorMessage.context-B4seO6gR.js";const C=b.div`
+  position: relative;
+  display: inline-block;
+
+  &::after {
+    content: "\\E5C5";
+    font-family: "Material Icons";
+    position: absolute;
+    right: 8px;
+    bottom: 8px;
+    font-size: 2.4rem;
+    line-height: 1;
+    color: var(--neutral600);
+    pointer-events: none;
+  }
+`,F=b.select`
+  ${c.Regular}
+  --input-width-sm: 120px;
+  --input-width-md: 240px;
+  --input-width-lg: 360px;
+
+  ${c.Border("input")}
+  background-color: var(--neutral50);
+  min-height: 40px;
+  width: 100%;
+  padding: 10px 40px 10px 12px;
+  font-size: 1.6rem;
+  line-height: 1;
+  color: var(--text-clr);
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  &::-ms-expand {
+    display: none;
+  }
+
+  &:focus {
+    border-color: var(--outline-clr-primary);
+    outline: var(--input-outline);
+  }
+`,r=_.forwardRef((a,y)=>{const[g,o,d]=R("select",["select","message","error"]),{children:k,onChange:n,size:A="medium",label:s,error:t,message:u,isDisabled:q=!1,isRequired:S=!1,isInvalid:i=!1,className:E,id:m=g,...x}=a,p=N({componentClassName:"aje-input__select",...a,selectors:{"data-disabled":q,"data-required":S,"data-invalid":i}});return e.jsxs("div",{...p,children:[s&&e.jsx(w,{htmlFor:m,children:s}),u&&e.jsx(z,{id:o,children:u}),e.jsx(C,{children:e.jsx(F,{id:m,"aria-describedby":i&&t?d:o,onChange:V=>n==null?void 0:n(V.target.value),ref:y,...x,children:p.children})}),i&&t&&e.jsx(j,{id:d,children:t})]})});try{r.displayName="Select",r.__docgenInfo={description:"Select Component. Simple wrapper around native `<select>`",displayName:"Select",props:{className:{defaultValue:null,description:`Add classes to the root element of the component.
+Refer to this for possible values: https://github.com/JedWatson/classnames#readme`,name:"className",required:!1,type:{name:"Argument | Argument[]"}},label:{defaultValue:null,description:`A visible label for the field. Labels are always Sentence case.
+If you do not provide a label, you should provide an aria-label or aria-labelledby attribute.`,name:"label",required:!1,type:{name:"ReactNode"}},id:{defaultValue:null,description:"Unique id for the component",name:"id",required:!1,type:{name:"string | undefined"}},onChange:{defaultValue:null,description:"",name:"onChange",required:!1,type:{name:"((value: SelectValue) => void) | undefined"}},size:{defaultValue:null,description:"Size of the component",name:"size",required:!1,type:{name:"enum",value:[{value:"undefined"},{value:'"small"'},{value:'"medium"'},{value:'"large"'},{value:'"auto"'},{value:'"full"'}]}},isDisabled:{defaultValue:null,description:`Field cannot be interacted with, should be de-emphasized in the UI
+@selector [data-disabled]`,name:"isDisabled",required:!1,type:{name:"boolean | undefined"}},error:{defaultValue:null,description:"Error message for the field",name:"error",required:!1,type:{name:"ReactNode"}},name:{defaultValue:null,description:"Name of the Field",name:"name",required:!1,type:{name:"string | undefined"}},message:{defaultValue:null,description:"For additional information (ex. date format mm/dd/yy)",name:"message",required:!1,type:{name:"ReactNode"}},isRequired:{defaultValue:null,description:`Field must be interacted with. Should be indicated in the UI
+@selector [data-required]`,name:"isRequired",required:!1,type:{name:"boolean | undefined"}},isInvalid:{defaultValue:null,description:"Field has an error. Should be made to look like an error.\nControls whether the value of `error` is displayed\n@selector [data-invalid]",name:"isInvalid",required:!1,type:{name:"boolean | undefined"}}}}}catch{}try{Selectcomponent.displayName="Selectcomponent",Selectcomponent.__docgenInfo={description:"Select Component. Simple wrapper around native `<select>`",displayName:"Selectcomponent",props:{className:{defaultValue:null,description:`Add classes to the root element of the component.
+Refer to this for possible values: https://github.com/JedWatson/classnames#readme`,name:"className",required:!1,type:{name:"Argument | Argument[]"}},label:{defaultValue:null,description:`A visible label for the field. Labels are always Sentence case.
+If you do not provide a label, you should provide an aria-label or aria-labelledby attribute.`,name:"label",required:!1,type:{name:"ReactNode"}},id:{defaultValue:null,description:"Unique id for the component",name:"id",required:!1,type:{name:"string | undefined"}},onChange:{defaultValue:null,description:"",name:"onChange",required:!1,type:{name:"((value: SelectValue) => void) | undefined"}},size:{defaultValue:null,description:"Size of the component",name:"size",required:!1,type:{name:"enum",value:[{value:"undefined"},{value:'"small"'},{value:'"medium"'},{value:'"large"'},{value:'"auto"'},{value:'"full"'}]}},isDisabled:{defaultValue:null,description:`Field cannot be interacted with, should be de-emphasized in the UI
+@selector [data-disabled]`,name:"isDisabled",required:!1,type:{name:"boolean | undefined"}},error:{defaultValue:null,description:"Error message for the field",name:"error",required:!1,type:{name:"ReactNode"}},name:{defaultValue:null,description:"Name of the Field",name:"name",required:!1,type:{name:"string | undefined"}},message:{defaultValue:null,description:"For additional information (ex. date format mm/dd/yy)",name:"message",required:!1,type:{name:"ReactNode"}},isRequired:{defaultValue:null,description:`Field must be interacted with. Should be indicated in the UI
+@selector [data-required]`,name:"isRequired",required:!1,type:{name:"boolean | undefined"}},isInvalid:{defaultValue:null,description:"Field has an error. Should be made to look like an error.\nControls whether the value of `error` is displayed\n@selector [data-invalid]",name:"isInvalid",required:!1,type:{name:"boolean | undefined"}}}}}catch{}const re={title:"Dropdowns/Selection/Select",component:r,parameters:{layout:"centered"},argTypes:{value:{control:"select",options:["value1","value2","value3"]},...I}},l={args:{label:"Select label",size:"medium",children:[e.jsx("option",{value:"value1",children:"Option 1"},"1"),e.jsx("option",{value:"value2",children:"Option 2"},"2"),e.jsx("option",{value:"value3",children:"Option 3"},"3")]}};var f,h,v;l.parameters={...l.parameters,docs:{...(f=l.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  args: {
+    label: "Select label",
+    size: "medium",
+    children: [<option value="value1" key="1">
+        Option 1
+      </option>, <option value="value2" key="2">
+        Option 2
+      </option>, <option value="value3" key="3">
+        Option 3
+      </option>]
+  }
+}`,...(v=(h=l.parameters)==null?void 0:h.docs)==null?void 0:v.source}}};const oe=["Primary"];export{l as Primary,oe as __namedExportsOrder,re as default};
